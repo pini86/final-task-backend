da0fbf497a3fdc43d719d0aefbb84d37a9f5b8d7
Mikhail Druzhinin <pini86@mail.ru>
2022-11-21 13:25:05 +0300

    fix: desc

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,18 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://pini86:6EFeUKbIVXfbEano@cluster0.3rp7pni.mongodb.net/managerApp');
+    await mongoose.connect(
+      'mongodb+srv://pini86:6EFeUKbIVXfbEano@cluster0.3rp7pni.mongodb.net/?retryWrites=true&w=majority'
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
